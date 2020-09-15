added gradient mask, fixed numbering

dev/data_utils.py
@@ -43,14 +43,15 @@ class augMask():
     def __init__(self,config_file):
         
         self.config_file = config_file
+
         with open(self.config_file, 'r') as config:
             self.cfg = yaml.safe_load(config)
-        self.submasks = self.cfg['submasks']
         self.dims = self.cfg['image_dimensions']
+        self.submasks = self.cfg['submasks']
         self.mask_list = [e for e in self.submasks if self.submasks[e]['use'] == True] #read in config file, create list of masks which user wants to run
         print('submasks: '+str(self.mask_list))
         self.mask = self.sub_mask_assemble()
-        #self.test_image = plt.imread('../data/2020-09-11_09-44-36-0.tiff')
+#        self.test_image = plt.imread('../data/2020-09-11_09-44-36-0.tiff')
         
 
     def sub_mask_assemble(self):
@@ -73,6 +74,8 @@ class augMask():
             out = self.scans(out)
         if 'standardize' in self.mask_list:
             out = self.standardize(out)
+        if 'gradient' in self.mask_list:
+            out = self.gradient(out)
         if 'speckle' in self.mask_list:
             out = self.speckle(out)
 
@@ -147,7 +150,28 @@ class augMask():
         return smart_wrapper
 
  
-        return None
+
+    def gradient(self, func):
+        print('gradient')
+        lower_bound = self.submasks['gradient']['params']['lower_bound']
+        def grad_wrap(*args, **kwargs):
+            theta = random.uniform(0, 2*np.pi)
+            strength = random.uniform(lower_bound, 1)
+            xx, yy = np.mgrid[0:1:np.complex(0,self.dims[0]), 0:1:np.complex(0,self.dims[1])]
+            xx = np.cos(theta)*xx+np.sin(theta)*yy
+            yy = np.cos(theta)*yy-np.sin(theta)*xx
+            mask = xx*yy
+            mask = (mask-mask.min())
+            mask = mask/mask.max()
+
+            mask = mask*(1-strength)
+            mask = mask+strength
+            return func(*args, **kwargs)*mask
+
+        return grad_wrap
+
+         
+
 
     def scans(self, func):
         print('scanning')
@@ -225,7 +249,7 @@ class augMask():
                 yCoord = random.randint(0,self.dims[1])
                 m1 = circle_mask(radius, xCoord, yCoord)
                 mask = mask+circle_mask(radius, xCoord, yCoord)
-            return np.clip(mask+func(*args, **kwargs), 0, 1)
+            return mask+func(*args, **kwargs)
 
         return circle_wrap
 

dev/genData.py
@@ -25,17 +25,18 @@ def decrossI(beta,image):
 
 def thermal_noise_sequence(n_imgs):
     current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-    number_decrosses = 4
-    number_augments = 4
-    simulation_iterations = 500
+    number_decrosses = 10
+    number_augments = 10
+    simulation_iterations = 200
     snapshots = 4 #doesn't really matter
-    image_dimensions = [256,256]
+    image_dimensions = [512,512]
     betaMax = 135
     betaMin = 45
     maxDefect = 100
+    minDefect = 1
     mask = data_utils.augMask('./maskTemplate.yaml')
-    for i in np.arange(0,n_imgs,number_decrosses+number_augments):
-        n_defects = random.randint(0,maxDefect)
+    for i in np.arange(0,n_imgs,number_decrosses*number_augments):
+        n_defects = random.randint(minDefect,maxDefect)
         t = Texture(n_defects, simulation_iterations, snapshots, image_dimensions)
         for j in np.arange(0,number_decrosses):
             beta = random.uniform(betaMin, betaMax)
@@ -43,7 +44,7 @@ def thermal_noise_sequence(n_imgs):
                 out_img = mask.mask(decrossI(beta, t.xy.snapshots['lattice'][-1]))
                 out_defect = t.xy.snapshots['defects'][-1]
 
-                plt.imsave('../data/'+current_time+'-t_{}.tiff'.format(i+j+k), out_img, cmap = 'gray')
+                plt.imsave('../data/'+current_time+'-t_{}.tiff'.format(i*number_decrosses*number_augments+j*number_augments+k), out_img, cmap = 'gray')
                 np.savetxt('../data/label_'+current_time+'-t_{}.dat'.format(i+j+k), out_defect)
 
 